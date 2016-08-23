f794648afd21ee93f1637369e3e5a7920199cc87
Sam West-Sooby <s.west.sooby@gmail.com>
2016-08-24 09:45:22 +1000

    pushing my fork into git

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources :events
   resources :hobbies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # getting the login form
+  get '/login', to: 'session#new'
+  # creating a session / logging in
+  post '/session', to: 'session#create'
+  # destroying a session / logging out
+  delete '/session', to: 'session#destroy'
 end
