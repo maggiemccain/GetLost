c424dee4a8c0a92415f222d288f2b69cfeeca8a7
Sam West-Sooby <s.west.sooby@gmail.com>
2016-08-25 17:08:00 +1000

    day 4 commit agains

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824001408) do
+ActiveRecord::Schema.define(version: 20160825070213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(version: 20160824001408) do
   create_table "hobbies", force: :cascade do |t|
     t.string   "sport"
     t.text     "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "image_photo"
   end
 
   create_table "users", force: :cascade do |t|
