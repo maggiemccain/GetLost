986c77ae8988495aea4251ade08f82fdde4248d5
Sam West-Sooby <s.west.sooby@gmail.com>
2016-08-26 09:05:10 +1000

    Friday morning first push

--- app/controllers/api/usersevents_controller.rb
+++ app/controllers/api/usersevents_controller.rb
@@ -5,7 +5,7 @@ module Api
     def create
       @join = Usersevent.new
       @join.user_id = current_user.id
-      @join.event_id = params[:event_id]
+      @join.event_id = params[:event_id] 
       @join.save
       join_count = Event.find(params[:event_id]).users.count
       attendee_name = User.find(current_user.id).name
