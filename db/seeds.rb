e3361f19066334a2d20092755d9d1750c28c77cb
Maggie McCain <maggie.mccain13@gmail.com>
2016-08-26 09:40:12 +1000

    seed conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -128,59 +128,7 @@ h5.sport = 'Fishing'
 h5.image_url = 'http://imgur.com/aQIT0EM.png'
 h5.image_photo = 'https://www.daf.qld.gov.au/__data/assets/image/0007/129742/Fisherman-fishing-at-sunset-.jpg'
 h5.save
->>>>>>> a9a568bbada7b33c1fb4859a4faa0051c6612355
 
-e = Event.new
-e.listing = 'Fishing on Yarra River'
-e.location = 'Yarra Bend Park'
-e.latitude = -37.7940
-e.longitude = 145.0101
-e.state = 'Victoria'
-e.image_url = 'http://www.streamsongresort.com/wp-content/uploads/2014/04/header-bass-fishing.jpg'
-e.attendees = 5
-e.hobby_id = 6
-e.description = 'Looking for fishing enthusiasts to join me on my paddle boat to fish in the Yarra River.  We will plan to meet at Yarra Bend Park entrance bright and early at 5am to get the best fish from this brown river!'
-e.date = '9/1/16'
-e.save
-
-e1 = Event.new
-e1.listing = 'Off Roading Expedition'
-e1.location = 'Yarra Valley'
-e1.latitude = -37.65689
-e1.longitude = 145.44729
-e1.state = 'Victoria'
-e1.image_url = 'https://www.google.com.au/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwijt7TGgdnOAhUDl5QKHTmNDm0QjBwIBA&url=http%3A%2F%2Fblog.chapmanchryslerjeep.com%2Ffiles%2F2013%2F02%2FJeep.jpg&bvm=bv.130731782,d.dGo&psig=AFQjCNHlNGJ3R12UsjY0stV29fyqclRdFw&ust=1472092363034186'
-e1.attendees = 3
-e1.hobby_id = 9
-e1.description = 'I love off roading in my new Jeep!  Would love company on an afternoon off roading excursion.  Maybe even a kangaroo siting!  Will send out meet up logistics a week before our adventure.'
-e1.date = '10/1/16'
-e1.save
-
-e = Event.new
-e.listing = 'Fishing on Yarra River'
-e.location = 'Yarra Bend Park'
-e.latitude = -37.7038
-e.longitude = 145.31010
-e.state = 'Victoria'
-e.image_url = 'http://www.streamsongresort.com/wp-content/uploads/2014/04/header-bass-fishing.jpg'
-e.attendees = 5
-e.hobby_id = 1
-e.description = 'Looking for fishing enthusiasts to join me on my paddle boat to fish in the Yarra River.  We will plan to meet at Yarra Bend Park entrance bright and early at 5am to get the best fish from this brown river!'
-e.date = '9/1/16'
-e.save
-
-e1 = Event.new
-e1.listing = 'Off Roading Expedition'
-e1.location = 'Yarra Valley'
-e1.latitude = -37.606898
-e1.longitude = 145.6472901
-e1.state = 'Victoria'
-e1.image_url = 'https://www.google.com.au/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwijt7TGgdnOAhUDl5QKHTmNDm0QjBwIBA&url=http%3A%2F%2Fblog.chapmanchryslerjeep.com%2Ffiles%2F2013%2F02%2FJeep.jpg&bvm=bv.130731782,d.dGo&psig=AFQjCNHlNGJ3R12UsjY0stV29fyqclRdFw&ust=1472092363034186'
-e1.attendees = 3
-e1.hobby_id = 2
-e1.description = 'I love off roading in my new Jeep!  Would love company on an afternoon off roading excursion.  Maybe even a kangaroo siting!  Will send out meet up logistics a week before our adventure.'
-e1.date = '10/1/16'
-e1.save
 
 #=================================================
 
